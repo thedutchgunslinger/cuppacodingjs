a52da0466b35ab71d43c5d8e5e2d6816e2d5d7df
Noah Beij <36964954+thedutchgunslinger@users.noreply.github.com>
2023-03-31 18:29:30 +0200

    added better pwa support and open md links in _blank

--- cuppacoding/pages/opdracht.js
+++ cuppacoding/pages/opdracht.js
@@ -56,6 +56,13 @@ const CodeBlock = (props) => {
 
 const components = {
   code: CodeBlock,
+  a: ({ node, children, ...props }) => {
+      if (props.href?.includes('http')) {
+        props.target = '_blank'
+        props.rel = 'noopener noreferrer'
+      }
+      return <a {...props}>{children}</a>
+    },
 };
 
 const markdown = "# Oefening: Check priemgetal \r\n\n## Omschrijving\n\nSchrijf een functie die bepaalt of een gegeven getal een priemgetal is. \nJe kunt gebruik maken van de `div(n)` functie om de delers van het getal op te vragen.\n\n## Opdracht\nImplementeer de functie `is_prime(n)` volgens onderstaande specificaties:\n\n```python\ndef is_prime(n):\n'''\nBepaal of gegeven getal een priemgetal is.\nArgs:\n\n (int): Een geheel getal.\nReturns:\nbool: True als het getal een priemgetal is, anders False.\n'''\n```\nHet algoritme om te bepalen of een getal een priemgetal is kan als volgt worden geïmplementeerd:\n1. Controleer of het getal n kleiner of gelijk is aan 1. Als dit het geval is, is het geen priemgetal.\n2. Controleer of het getal n deelbaar is door 2 of 3. Als dit het geval is, is het geen priemgetal.\n3. Controleer of het getal n deelbaar is door een oneven getal i tussen 5 en de vierkantswortel van n (afgerond naar beneden). Als dit het geval is, is het geen priemgetal. Je kunt hiervoor gebruik maken van de sqrt() functie uit de math module.\nTip: het hergebruiken van andere functies wordt aangemoedigd!\nTip: als je twijfelt over de werking van de functie, kun je een paar eenvoudige tests uitvoeren om\nte controleren of de uitvoer correct is.\n## Bronnen\n\n* [Math-module in python](https://www.w3schools.com/python/python_math.asp)\n* [Wikipedia over priemgetallen](https://nl.wikipedia.org/wiki/Priemgetal)";
